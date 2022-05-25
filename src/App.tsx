e73cbe8d89035553b3c2e61cfdc8921306c94592
Ponyville <favoritePonyville@gmail.com>
2022-05-25 23:14:42 +0400

    fixet the bug in ts: cannot find module or its corresponding type declarations.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.scss';
+import Icons from './img/icons.svg';
 
 function App() {
+  console.log(Icons);
+  
   return (
     <div className="App">
-      
+      <svg>
+        <use href={`${Icons}#delete`}/>
+      </svg>
     </div>
   );
 }
